5d380eea7b02b4ff3f7008990128f8b294b08f55
Edgar Perez <edgarp.dev@gmail.com>
2024-02-13 08:22:31 -0600

    Added sns and db resources

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
         -e|--env)
-            ENVIRONMENT="$2"
+            ENV="$2"
             shift
             shift
             ;;
@@ -43,18 +43,18 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [ -z "$ENVIRONMENT" ] || [ -z "$PROFILE" ]; then
+if [ -z "$ENV" ] || [ -z "$PROFILE" ]; then
     echo "Error: -e and -p parameters are mandatory."
     show_help
     exit 1
 fi
 
-if [ "$ENVIRONMENT" != "dev" ] && [ "$ENVIRONMENT" != "prod" ]; then
+if [ "$ENV" != "dev" ] && [ "$ENV" != "prod" ]; then
     echo "Error: Invalid environment. Allowed values are dev and prod."
     exit 1
 fi
 
-if [ "$ENVIRONMENT" == "prod" ]; then
+if [ "$ENV" == "prod" ]; then
     read -p "Are you sure you want to deploy to prod environment? (y/N) " confirm
     if [ "$confirm" != "y" ]; then
         echo "Deployment to prod environment aborted."
@@ -72,4 +72,36 @@ npm install --production
 cd ../..
 ls
 
-echo "Deploying to $ENVIRONMENT environment in region $REGION using profile $PROFILE..."
+echo "Deploying to $ENV environment in region $REGION using profile $PROFILE..."
+
+
+BUCKET_NAME="tag-processor-artifacts-$ENV"
+if awsv2 s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+    echo "Bucket $BUCKET_NAME already exists."
+else
+    echo "Bucket $BUCKET_NAME does not exist. Creating..."
+    awsv2 s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --profile "$PROFILE"
+    echo "Bucket $BUCKET_NAME created."
+fi
+
+sam package --output-template-file ./output.yaml \
+    --s3-bucket "$BUCKET_NAME" \
+    --profile "$PROFILE" \
+    --region "$REGION" \
+    --template ./cloudformation/template.yaml
+
+STACK_NAME="tag-processor-stack-$ENV"
+sam deploy --capabilities CAPABILITY_IAM \
+    --no-confirm-changeset \
+    --parameter-overrides ENV=dev \
+    --profile "$PROFILE" \
+    --region "$REGION" \
+    --s3-bucket "$BUCKET_NAME" \
+    --stack-name "$STACK_NAME" \
+    --template ./output.yaml
+
+
+echo "Cleaning build files"
+rm -rf ./tag-processor/build
+rm output.yaml
+
