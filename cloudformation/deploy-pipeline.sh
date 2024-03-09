cf4b2b4452e7a7f23e230a96329e1d5119b5b47c
Edgar Perez <edgarp.dev@gmail.com>
2024-03-09 12:32:40 -0600

    [NEW] Added pipeline resource and pipeline deploy script

---
+++ cloudformation/deploy-pipeline.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+REGION="us-east-1"
+
+show_help() {
+    echo "Usage: deploy.sh [OPTIONS]"
+    echo "Deploy pipeline to specified environment."
+    echo ""
+    echo "Options:"
+    echo "  -e, --env <environment>   Environment to deploy (dev or prod)"
+    echo "  -p, --profile <profile>   AWS profile"
+    echo "  -r, --region <region>     AWS region"
+    echo "  -h, --help                Show this help message"
+}
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -e|--env)
+            ENV="$2"
+            shift
+            shift
+            ;;
+        -p|--profile)
+            PROFILE="$2"
+            shift
+            shift
+            ;;
+        -r|--region)
+            REGION="$2"
+            shift
+            shift
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$ENV" ] || [ -z "$PROFILE" ]; then
+    echo "Error: -e and -p parameters are mandatory."
+    show_help
+    exit 1
+fi
+
+if [ "$ENV" != "prod" ]; then
+    echo "Error: Invalid environment. Allowed value is prod."
+    exit 1
+fi
+
+echo "Deploying pipeline"
+STACK_NAME="tag-scraper-pipeline-$ENV"
+awsv2 cloudformation deploy \
+    --template-file ./pipeline.yaml \
+    --stack-name $STACK_NAME \
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameter-overrides Env=prod GitHubOwner=edgarp-dev GitHubRepo=tag-scraper GitHubBranch=main \
+    --profile $PROFILE \
+    --region $REGION
