905e65f721b4eceafbb30e8dc5b6e9a91779c6c6
Edgar Perez <edgarp.dev@gmail.com>
2024-03-29 14:33:15 -0600

    [UPDATE] Added pipeline mode flag

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 REGION="us-east-1"
+PIPELINE_MODE=false  # Default value indicating --pipeline is not set
 
 show_help() {
     echo "Usage: deploy.sh [OPTIONS]"
@@ -10,6 +11,7 @@ show_help() {
     echo "  -e, --env <environment>   Environment to deploy (dev or prod)"
     echo "  -p, --profile <profile>   AWS profile"
     echo "  -r, --region <region>     AWS region"
+    echo "  --pipeline                Activate pipeline mode"
     echo "  -h, --help                Show this help message"
 }
 
@@ -31,6 +33,10 @@ while [[ $# -gt 0 ]]; do
             shift
             shift
             ;;
+        --pipeline)
+            PIPELINE_MODE=true
+            shift
+            ;;
         -h|--help)
             show_help
             exit 0
@@ -43,8 +49,15 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [ -z "$ENV" ] || [ -z "$PROFILE" ]; then
-    echo "Error: -e and -p parameters are mandatory."
+
+if [ -z "$ENV" ]; then
+    echo "Error: -e parameter is mandatory."
+    show_help
+    exit 1
+fi
+
+if [ "$PIPELINE_MODE" = false ] && [ -z "$PROFILE" ]; then
+    echo "Error: -p parameter is mandatory when not in pipeline mode."
     show_help
     exit 1
 fi
@@ -55,11 +68,20 @@ if [ "$ENV" != "dev" ] && [ "$ENV" != "prod" ]; then
 fi
 
 if [ "$ENV" == "prod" ]; then
-    read -p "Are you sure you want to deploy to prod environment? (y/N) " confirm
-    if [ "$confirm" != "y" ]; then
-        echo "Deployment to prod environment aborted."
-        exit 1
+    if [ "$PIPELINE_MODE" = false ]; then
+        read -p "Are you sure you want to deploy to prod environment? (y/N) " confirm
+        if [ "$confirm" != "y" ]; then
+            echo "Deployment to prod environment aborted."
+            exit 1
+        fi
     fi
+fi
+
+
+if [ "$PIPELINE_MODE" = true ]; then
+    echo "Pipeline mode activated."
+else
+    echo "Standard deployment mode."
 fi
 
 echo "Compiling lambdas"
@@ -79,29 +101,36 @@ cd ./build
 npm install --production
 cd ../..
 
-echo "Deploying to $ENV environment in region $REGION using profile $PROFILE..."
-
+echo "Deploying to $ENV environment in region $REGION..."
 
 BUCKET_NAME="tag-processor-artifacts-$ENV"
+
+PROFILE_FLAG=""
+if [ "$PIPELINE_MODE" = false ]; then
+    PROFILE_FLAG="--profile $PROFILE"
+    echo "using profile $PROFILE..."
+fi
+
 if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
     echo "Bucket $BUCKET_NAME already exists."
 else
     echo "Bucket $BUCKET_NAME does not exist. Creating..."
-    aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" --profile "$PROFILE"
+    aws s3api create-bucket --bucket "$BUCKET_NAME" --region "$REGION" $PROFILE_FLAG
     echo "Bucket $BUCKET_NAME created."
 fi
 
 sam package --output-template-file ./output.yaml \
     --s3-bucket "$BUCKET_NAME" \
-    --profile "$PROFILE" \
+    $PROFILE_FLAG \
     --region "$REGION" \
     --template ./cloudformation/template.yaml
 
 STACK_NAME="tag-processor-stack-$ENV"
+
 sam deploy --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --no-confirm-changeset \
     --parameter-overrides ENV=dev \
-    --profile "$PROFILE" \
+    $PROFILE_FLAG \
     --region "$REGION" \
     --s3-bucket "$BUCKET_NAME" \
     --stack-name "$STACK_NAME" \
