1155345571f89b61eec9e8a797518ae52e110507
edgarp-dev <edgarp.dev@gmail.com>
2025-03-04 08:52:06 -0600

    [UPDATE] Fixed errors

--- web-scraper/src/adapters/TelegramAdapter.ts
+++ web-scraper/src/adapters/TelegramAdapter.ts
@@ -11,28 +11,37 @@ export default class TelegramAdapter implements NotificationService {
   }
 
   public async notifyError(message: string, photoPath: string): Promise<void> {
+    const screenshotExists = fs.existsSync(photoPath);
+
     const telegramApiUrl = 'https://api.telegram.org/bot';
     const channelId = '-1002253352797';
-    const url = `${telegramApiUrl}${this.telegramToken}/sendPhoto`;
+    const url = `${telegramApiUrl}${this.telegramToken}/${screenshotExists ? 'sendPhoto' : 'sendMessage'}`;
 
     const currentDate = new Date();
     const formattedDate = this.formatDate(currentDate);
     const errorMessage = `Error message: ${message}.\nDate: ${formattedDate}`;
 
     try {
-      const form = new FormData();
-      form.append('chat_id', channelId);
-      form.append('caption', errorMessage);
-
-      if (fs.existsSync(photoPath)) {
+      if (screenshotExists) {
+        console.log('Sending message with screenshot...');
+        const form = new FormData();
+        form.append('chat_id', channelId);
+        form.append('caption', errorMessage);
         form.append('photo', fs.createReadStream(photoPath));
+
+        const response = await axios.post(url, form, {
+          headers: form.getHeaders()
+        });
+
+        console.log('Message sent successfully:', response.data);
+      } else {
+        const response = await axios.post(url, {
+          chat_id: channelId,
+          text: errorMessage
+        });
+
+        console.log('Message sent successfully:', response.data);
       }
-
-      const response = await axios.post(url, form, {
-        headers: form.getHeaders()
-      });
-
-      console.log('Message sent successfully:', response.data);
     } catch (error) {
       console.error('Error sending the message:', (error as Error).message);
     }
